e3fcddf3ae1be9b521b495e971e778b37f83efe9
leejw05 <zeta050525@gmail.com>
2022-05-06 01:24:30 +0900

    시퀀스 추가

--- db/index.js
+++ db/index.js
@@ -16,11 +16,13 @@ export default {
       Title: "What is Node js?...",
       Desc: "Node js is .....",
       Writer: "zeta1232",
+      id: 1,
     },
     {
       Title: "What is Python?...",
       Desc: "Python is .....",
       Writer: "First1",
+      id: 2,
     },
   ],
 };
